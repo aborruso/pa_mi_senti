8870b72140aa6a0c5f8576ce0317f817cf3968b8
aborruso <aborruso@gmail.com>
2025-10-18 17:26:35 +0200

    fix: update BASE_PATH in astro.config.mjs and adjust dev script in package.json

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
 
-const BASE_PATH = process.env.ASTRO_BASE ?? "/";
+const BASE_PATH = process.env.ASTRO_BASE ?? "/pa_mi_senti/";
 
 export default defineConfig({
   site: "https://aborruso.github.io",
