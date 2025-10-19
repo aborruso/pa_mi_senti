a472ae11b77e55b4ed38dd11d44b7764961e202c
aborruso <aborruso@gmail.com>
2025-10-19 11:21:50 +0200

    feat: rimuovere il riferimento a triple slash per evitare errori di build

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,4 +1,3 @@
-/// <reference path="../.astro/types.d.ts" />
 // DO NOT add triple slash reference - it causes build errors
 // Astro types are imported via the statement below
 import "astro/client";
